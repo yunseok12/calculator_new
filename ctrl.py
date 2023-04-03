ee0a75c3ca2e4f2496938d4b23f66c75397d3384
“developer1” <dev1@email.com>
2023-04-03 10:16:15 +0900

    Modify sum function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -11,4 +11,7 @@ class Control:
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
     def sum(self, a, b):
-        return a+b
+        try:
+            return a+b
+        except:
+            return "Calculation Error"

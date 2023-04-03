ec426c4de766f39d0cc236c5816728483fbdbc39
“developer1” <dev1@email.com>
2023-04-03 10:46:40 +0900

    Add power operation to combobox

--- ui.py
+++ ui.py
@@ -27,7 +27,7 @@ class View(QWidget):
         self.le2.setAlignment(QtCore.Qt.AlignRight)
         
         self.cb = QComboBox(self)
-        self.cb.addItems(['+', '-', '*', '/'])
+        self.cb.addItems(['+', '-', '*', '/', '^'])
         
         hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
@@ -54,8 +54,6 @@ class View(QWidget):
         
     def setDisplay(self, text):
         self.te1.appendPlainText(text)
-    def setDisplay(self):
-        self.te1.appendPlainText("Button clicked!")
         
     def clearMessage(self):
         self.te1.clear()

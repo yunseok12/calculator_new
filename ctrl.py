0950895b2cfb351c86d82685b229eff5abbbec83
developer2 <clionelove@naver.com>
2023-04-03 11:17:42 +0900

    Modify ctrl.py to connect sum operation to display

--- ctrl.py
+++ ctrl.py
@@ -4,14 +4,19 @@ class Control:
         self.connectSignals()
         
     def calculate(self):
-        pass
+        num1 = float(self.view.le1.text())
+        num2 = float(self.view.le2.text())
+        operator =self.view.cb.currentText()
+        
+        if operator =='+':
+            return f'{num1} + {num2} = {self.sum(num1, num2)}'
+        
+        else:
+            return "Calculation Error"
         
     def connectSignals(self):
-        self.view.btn1.clicked.connect(self.calculate)
+        self.view.btn1.clicked.connect(lambda: self.view.setDisplay(self.calculate()))
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
     def sum(self, a, b):
-        try:
             return a+b
-        except:
-            return "Calculation Error"

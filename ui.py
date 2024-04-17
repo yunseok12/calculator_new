25c1ddb4ab5c12dcfcec13da111af7ef94c7cfa7
developer1 <diddbstjr5537@naver.com>
2024-04-18 03:27:26 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -15,19 +15,17 @@ class View(QWidget):
         self.te1 = QPlainTextEdit()
         self.te1.setReadOnly(True)
         
-        self.btn1=QPushButton('Calc',self)
+        self.btn1=QPushButton('Message',self)
         self.btn2=QPushButton('Clear',self)
         
         self.le1=QLineEdit('0',self)
         self.le1.setAlignment(QtCore.Qt.AlignRight)
-        self.le1.setFocus(True)
-        self.le1.selectAll()
         
         self.le2=QLineEdit('0',self)
         self.le2.setAlignment(QtCore.Qt.AlignRight)
         
         self.cb = QComboBox(self)
-        self.cb.addItems(['+', '-', '*', '/', '^'])
+        self.cb.addItems(['+', '-', '*', '/'])
         
         hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
@@ -52,8 +50,8 @@ class View(QWidget):
         self.resize(256,256)
         self.show()
         
-    def setDisplay(self, text):
-        self.te1.appendPlainText(text)
+    def activateMessage(self):
+        self.te1.appendPlainText("Button clicked!")
         
     def clearMessage(self):
         self.te1.clear()
